836b9ba4e242ee17c5391fbb72059594598a568f
Jesus Imery <yisas_0929@hotmail.com>
2018-01-27 12:04:46 -0500

    #8 #7 Animated doors

---
+++ Assets/Scripts/Objects/Door.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Door : MonoBehaviour {
+
+    public Animator animator;
+
+    public void ToggleOpen()
+    {
+        animator.SetTrigger("ToggleOpen");
+    }
+}
