390e8c245eddc0d96fa7568bb728522154bed15c
Sabz <sabrina.d.mello@hotmail.com>
2018-01-28 13:28:30 -0500

    #10 added a simple soundtrack manager to the lvl 1 to 3

---
+++ Assets/Scripts/SoundtrackManagment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundtrackManagment : MonoBehaviour {
+
+    AudioSource source;
+    float volumeControl = 0.5f;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    void Start () 
+    {
+        source.volume = volumeControl;
+    }
+	
+	void Update () { }
+
+
+
+}
