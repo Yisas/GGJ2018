26c0a11fea9c0dfb7e74feccf821692f34c7d28b
Vladimir de la Cruz <vladimirdlc@gmail.com>
2018-01-28 06:47:58 -0500

    Added Tutorial dialog popup on trigger
    Added transparent material

---
+++ Assets/Scripts/GUI/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    public static UIManager Instance;
+    public GameObject dialog;
+    public Text textBox;
+
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+	}
+
+    public void showText(string text)
+    {
+        textBox.text = text;
+        dialog.SetActive(true);
+    }
+
+    public void hideText()
+    {
+        dialog.SetActive(false);
+    }
+}
--- Assets/Scripts/Tutorial/TutorialTrigger.cs
+++ Assets/Scripts/Tutorial/TutorialTrigger.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TutorialTrigger : MonoBehaviour {
+    [TextArea(3, 10)]
+    public string myText = "This text will appear in a text area";
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        UIManager.Instance.showText(myText);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        UIManager.Instance.hideText();
+    }
 }
